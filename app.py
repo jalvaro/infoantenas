0820f8310f2423e97b353bac6f0bb2b4fb606f01
Jordi Alvaro <jordi.alvaro.arques@gmail.com>
2018-11-19 23:58:05 +0100

    Jsonify response

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def healthcheck():
 @app.route('/antennas')
 def get_anthens_info():
     r = requests.get('https://geoportal.minetur.gob.es/VCTEL/infoantenasGeoJSON.do?idCapa=null&bbox=-3.3525553686015%2C41.838103675076%2C-3.3428779584757%2C41.844144009778&zoom=4')
-    return r.text
+    return jsonify(r.json())
 
 def get_app():
     return app
