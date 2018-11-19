61330d9c21bea06618d0f3638d9c3e2f9c4387ca
Jordi Alvaro <jordi.alvaro.arques@gmail.com>
2018-11-19 04:04:39 +0100

    Add request to infoantenas

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, jsonify
-
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def healthcheck():
     return jsonify(status='healthy!')
+
+@app.route('/infoantenas')
+def get_anthens_info():
+    return requests.get('https://geoportal.minetur.gob.es/VCTEL/infoantenasGeoJSON.do?idCapa=null&bbox=-3.3525553686015%2C41.838103675076%2C-3.3428779584757%2C41.844144009778&zoom=4').content
 
 def get_app():
     return app
