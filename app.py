a76b57a5180f1f089811b1d2b9deda071af41afd
Jordi Alvaro <jordi.alvaro.arques@gmail.com>
2018-11-19 03:49:53 +0100

    Testing app

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask, jsonify
 
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route('/')
-    def healthcheck():
-        return jsonify(status='healthy!')
-
-    return app
+@app.route('/')
+def healthcheck():
+    return jsonify(status='healthy!')
 
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(port=5000)
