87932b84c6a3f1936e6e28b5316e638d9fced9c8
Jordi Alvaro <jordi.alvaro.arques@gmail.com>
2018-11-20 18:22:40 +0100

    Enabling CORS

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 @app.route('/')
 def healthcheck():
