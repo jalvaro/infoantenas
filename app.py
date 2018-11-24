ab3401ec7594f5120ab158843782d3cdb6d15466
Jordi Alvaro <jordi.alvaro.arques@gmail.com>
2018-11-24 13:04:21 +0100

    Allow passing rectange in order to define an area

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
 
@@ -10,9 +10,47 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 def healthcheck():
     return jsonify(status='healthy!')
 
+def get_rectangle(request):
+    return {
+        'point1': {
+            'lat': request.args['lat1'],
+            'lng': request.args['lng1']
+        },
+        'point2': {
+            'lat': request.args['lat2'],
+            'lng': request.args['lng2']
+        }
+    }
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def is_point_malformed(point):
+    return point is None or point['lat'] is None or point['lng'] is None or not is_number(point['lat']) or not is_number(point['lng'])
+
 @app.route('/antennas')
 def get_anthens_info():
-    r = requests.get('https://geoportal.minetur.gob.es/VCTEL/infoantenasGeoJSON.do?idCapa=null&bbox=-3.3525553686015%2C41.838103675076%2C-3.3428779584757%2C41.844144009778&zoom=4')
+    print("test")
+    rectangle = get_rectangle(request)
+
+    if rectangle is None or is_point_malformed(rectangle['point1']) or is_point_malformed(rectangle['point2']):
+        return jsonify({})
+
+    point1 = rectangle['point1']
+    point2 = rectangle['point2']
+
+    bbox = ','.join([point1['lng'], point1['lat'], point2['lng'], point2['lat']])
+    payload = {
+        'idCapa': 'null',
+#        'bbox': '-3.3525553686015,41.838103675076,-3.3428779584757,41.844144009778',
+        'bbox': bbox,
+        'zoom': 4
+    }
+    r = requests.get('https://geoportal.minetur.gob.es/VCTEL/infoantenasGeoJSON.do', params=payload)
     return jsonify(r.json())
 
 def get_app():
